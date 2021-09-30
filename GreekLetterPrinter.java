0bdb4823c669badccafeadfb0b41e4892dcea041
Kevin Lo <kevin.lo@sjsu.edu>
2021-09-30 13:28:31 -0700

    first commit

---
+++ GreekLetterPrinter.java
@@ -0,0 +1,20 @@
+/**
+ * Print some Greek letters in a given format.
+ *
+ * @author Kevin Lo
+ * @version 2020-08-25
+ */
+public class GreekLetterPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("|*******|");
+        System.out.println("|** \u03B1 **|");
+        System.out.println("|** \u03B2 **|");
+        System.out.println("|** \u03C0 **|");
+        System.out.println("|** \u03A3 **|");
+        System.out.println("|*******|");
+        
+        
+    }
+}
